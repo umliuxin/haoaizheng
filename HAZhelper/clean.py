8fe0e0d9f227a1d337ae3ecb154f59ba4f9e7b8f
Xin Liu <umliuxin@umich.edu>
2014-02-27 23:51:58 -0500

    Spy Question Set V2

--- HAZhelper/clean.py
+++ HAZhelper/clean.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-rfile=open('spy.txt','rU')
+rfile=open('spy1.txt','rU')
 data=rfile.readlines()
 for line in data:
-	string="array('"+line.strip("\n").split(",")[0]+"','"+line.strip("\n").split(",")[1].strip('\t').strip(' ')+"'),"
+	string="array('"+line.strip("\n").split("\t")[0]+"','"+line.strip("\n").split("\t")[1].strip('\t').strip(' ')+"'),"
 	print string
-	 
